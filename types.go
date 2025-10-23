93d3fba7a2185702b5065dd67d7defe0ca978f40
viniciusaloise <vini.aloise.silva@gmail.com>
2025-10-22 21:49:15 -0300

    fix: search for SMTP_HOST env var instead of SMTP_SERVER

--- types.go
+++ types.go
@@ -1,7 +1,7 @@
 package logger
 
 type SMTP struct {
-	Server   string   `toml:"server" env:"SMTP_SERVER"`
+	Server   string   `toml:"server" env:"SMTP_HOST"`
 	Port     int      `toml:"port" env:"SMTP_PORT"`
 	Username string   `toml:"username" env:"SMTP_USERNAME"`
 	Password string   `toml:"password" env:"SMTP_PASSWORD"`

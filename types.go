3dff7ddf4d7938c639bd3b83053ac3f259d6ee36
viniciusaloise <vini.aloise.silva@gmail.com>
2025-10-22 21:20:29 -0300

    feat: add new tags to allow env configuration

--- types.go
+++ types.go
@@ -1,12 +1,12 @@
 package logger
 
 type SMTP struct {
-	Server   string   `toml:"server"`
-	Port     int      `toml:"port"`
-	Username string   `toml:"username"`
-	Password string   `toml:"password"`
-	From     string   `toml:"from"`
-	To       []string `toml:"to"`
+	Server   string   `toml:"server" env:"SMTP_SERVER"`
+	Port     int      `toml:"port" env:"SMTP_PORT"`
+	Username string   `toml:"username" env:"SMTP_USERNAME"`
+	Password string   `toml:"password" env:"SMTP_PASSWORD"`
+	From     string   `toml:"from" env:"SMTP_FROM"`
+	To       []string `toml:"to" env:"SMTP_TO"`
 }
 
 type Notification struct {
